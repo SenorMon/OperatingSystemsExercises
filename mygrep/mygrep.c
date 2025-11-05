20afb9fa978594323652df78344cc5f478ed5909
SirMon <>
2025-11-05 16:30:45 +0100

    added comments and fixed bugs

--- mygrep/mygrep.c
+++ mygrep/mygrep.c
@@ -1,5 +1,3 @@
-#define _POSIX_C_SOURCE 200809L
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -8,6 +6,12 @@
 
 #include "grep.h"
 
+
+/**
+
+    Allocate memory for default option_holder struct. 
+    @return option_holder struct initialized 
+*/
 option_holder* createOptionHolder(){
     option_holder* options = malloc(sizeof(option_holder));
     options->keyword = NULL;
@@ -19,6 +23,15 @@ option_holder* createOptionHolder(){
 }
 void writeToFile(char* buff, char* output_file);
 
+/**
+
+    The following function takes the option_holder struct as input
+    and prints the selected options such as keyword or 
+    if option i was selected to the terminal.
+
+    @param options option_holder struct 
+
+*/
 void printSelectedOptions(option_holder* options){
 
     printf("Keyword: %s\n", options->keyword);
@@ -34,39 +47,92 @@ void printSelectedOptions(option_holder* options){
     if(options->files == NULL){
         printf("Reading from standard input... \n");
     }else{
-        printf("Reading from selected files...\n");
+        printf("Reading from selected files... \n");
     }
 
 }
 
+/**
+
+    The following function allocates memory to the option_holder struct
+    in order to save the files to the array.
+
+    @param number_of_files the number of files that need to be put into the array
+    @param array_of_files an array of strings that needs to be stored in options
+    @param options the option_holder struct 
+
+*/
+void allocateFileArrayMemory(int number_of_files, char** array_of_files, option_holder* options){
+    if(number_of_files >= 1){
+        options->files = malloc(sizeof(options->files) * number_of_files);
+        
+        for(int i = 0; i < number_of_files; i++){
+            options->files[i] = array_of_files[i];
+        }
+    }
+}
+
+
+/**
+
+    The following function uses an input string and formats it to lower case.
+    The input string is not altered a copy is made.  
+
+    @param string any string
+    @return a copy of the input string which is now in lower case 
+
+ */
+char *copyToLowercase(char* string){
+    int size = strlen(string);
+    char *copy = malloc(size + 1); //allocating memory and taking null terminator into consideration 
+
+    if(!copy) return NULL;
+
+    for(int i = 0; i < size; i++){
+        copy[i] = tolower(string[i]);
+    }
+
+    copy[size] = '\0'; //adding null terminator 
+
+    return copy;
+}
+
+/**
+
+    The following returns an integer > 0 if the keyword
+    was found in the input string.
+    
+    @param buff input string
+    @param keyword keyword to search in buff 
+    @param opt_i opt_i > 0 -> case insensitive search 
+*/
 int hasSubstring(char* buff, char* keyword, int opt_i){
-
-
-    char string_buff[strlen(buff)] ;
-    memcpy(string_buff, buff, strlen(buff));
-
-
-
-
     if(opt_i > 0){
-        for(long unsigned int i = 0; i < strlen(keyword); i++){
-            keyword[i] = tolower(keyword[i]);
-        }
+        char* lower_buff = copyToLowercase(buff);
+        char* lower_keyword = copyToLowercase(keyword);
+        
+        char* searchedString;
+        searchedString = strstr(lower_buff, lower_keyword);
 
-        for(long unsigned int i = 0; i < strlen(string_buff); i++){
-            string_buff[i] = tolower(string_buff[i]);
-        }
+        return (searchedString != NULL);
      }
 
-
-
     char* searchedString;
-    searchedString = strstr(string_buff, keyword);
+    searchedString = strstr(buff, keyword);
 
     return (searchedString != NULL);
 }
 
+/**
 
+    The following function is called when the program is executed.
+    If there is an output file specified it writes it to the file.
+    If not then the stdout is used. 
+
+    @param buff input string (either user input or file input)
+    @param options the option_holder struct 
+
+*/
 void programOutput(char* buff, option_holder* options){
 
     if(hasSubstring(buff, options->keyword, options->opt_i)){
@@ -78,8 +144,14 @@ void programOutput(char* buff, option_holder* options){
     }
 }
 
+/**
 
+    Appending output to file. If file isn't found it is created. 
 
+    @param buff String that is appended to the output file
+    @param output_filw path to the output file. 
+
+*/
 void writeToFile(char* buff, char* output_file){
     FILE* fptr;
 
@@ -95,31 +167,39 @@ void writeToFile(char* buff, char* output_file){
     fclose(fptr);
 }
 
+/**
 
+    The following function reads from standard input (terminal).
+    This might need some rework to allow any length of data. 
 
+    @param options option_holder struct 
 
+*/
 void readingFromSTDIN(option_holder* options){
-        while(1){
-            char buff[256];
-            fgets(buff, sizeof(buff), stdin);
-            
+        char buff[256];
+        while(fgets(buff, sizeof(buff), stdin) != NULL){ 
             programOutput(buff, options);
         }
 }
 
+/**
 
+    The following function is called to start the grep
+    and using options to determine how the program behaves.
+
+    @param options option_holder struct
+*/
 void startMyGrep(option_holder* options){
     if(options->files == NULL){
         readingFromSTDIN(options);
     }
 }
 
-
-
-
 int main(int argc, char** argv){
-    option_holder* option = createOptionHolder();
+    option_holder* option = createOptionHolder(); //initializing option_holder
 
+
+    //Parsing options
     int opt;
     while((opt = getopt(argc, argv, ":io:")) != -1){
         switch(opt){
@@ -138,17 +218,30 @@ int main(int argc, char** argv){
         }
     }
 
+    //Parsing keywords
     option->keyword = argv[optind];
-
+    optind++;
 
     if(option->keyword == NULL){
         printf("Error occured: The keyword must not be NULL!\n");
         return EXIT_FAILURE;
     }
 
+    //Parsing files if there are any 
+    int number_of_files = argc - optind;
 
-
+    if(number_of_files > 0){
+        char *files[number_of_files];
+        for(int i = 0; optind < argc; optind++){
+            files[i] = argv[optind];
+            i++;
+        }
+        
+        allocateFileArrayMemory(number_of_files, files, option);
+    }
+    
     startMyGrep(option);
-
     free(option);
+
+    return EXIT_SUCCESS;
 }
